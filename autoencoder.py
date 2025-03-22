49b88d55c6b46f8be60de0d18c2e8a270c51f3d0
Richard Koulen <rkoulen3@gatech.edu>
2025-03-22 15:01:35 -0400

    Huge model (1GB), more terrible results

--- autoencoder.py
+++ autoencoder.py
@@ -1,31 +1,40 @@
+from typing import Tuple, Callable
+
 from dataloader import LeafsnapDataset
 import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 
-class NaiveAutoencoder(nn.Module):
-    def __init__(self, num_feats, learning_transform):
-        # init
+class _Autoencoder(nn.Module):
+    def __init__(self, input_shape:Tuple, learning_transform:Callable): # call this
         super().__init__()
-        self.num_feats = num_feats
+
+        assert isinstance(input_shape, tuple), 'Input shape must be a tuple.'
+        assert len(input_shape) == 4, 'Input shape must be (B, C, H, W).'
+
+        self.input_shape = input_shape
         self.learning_transform = learning_transform
+
+    def embed(self, x): # override this
+        raise NotImplementedError
+
+class NaiveAutoencoder(_Autoencoder):
+    def __init__(self, input_shape:Tuple, embedded_dim:int, learning_transform:Callable=lambda x: x):
+        # init
+        super().__init__(input_shape, learning_transform)
+        self.vector_dim = self.input_shape[1] * self.input_shape[2] * self.input_shape[3]
+        self.embedded_dim = embedded_dim
 
         # architecture
         self.encoder = nn.Sequential(
-            nn.Linear(self.num_feats, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32),
+            nn.Flatten(), # squash (C, H, W)
+            nn.Linear(self.vector_dim, self.embedded_dim),
             nn.ReLU(),
         )
         self.decoder = nn.Sequential(
-            nn.Linear(32, 64),
-            nn.ReLU(),
-            nn.Linear(64, 128),
-            nn.ReLU(),
-            nn.Linear(128, self.num_feats),
+            nn.Linear(self.embedded_dim, self.vector_dim),
             nn.Sigmoid(),
+            nn.Unflatten(dim=1, unflattened_size=self.input_shape[1:]), # unsquash (C, H, W)
         )
 
     def embed(self, x):
@@ -36,7 +45,7 @@ class NaiveAutoencoder(nn.Module):
         out = self.decoder(out)
         return out
 
-def train_AE_model(model:nn.Module, dataset:LeafsnapDataset, epochs:int, learning_rate:float):
+def train_AE_model(model:_Autoencoder, dataset:LeafsnapDataset, epochs:int, learning_rate:float):
     optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
     criterion = nn.MSELoss() #MSE loss is the only appropriate loss for AE
 
